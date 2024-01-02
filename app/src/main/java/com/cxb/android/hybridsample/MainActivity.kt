a396fc72f2034bf091be45e9c5c7d4e94b3bc788
BenChan <1079004837@qq.com>
2024-01-03 00:52:30 +0800

    首次搭建混合开发项目成功

---
+++ app/src/main/java/com/cxb/android/hybridsample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.cxb.android.hybridsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<AppCompatButton>(R.id.tv_flutter).setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            );
+        }
+    }
+}
